cd7976f3be63657ccb48956f202eec49dd5b4a2d
Boris <boris.chen@kdis.com>
2017-10-10 17:08:42 +1100

    create the reactive form and sync it with the Html template

--- src/pages/edit-recipe/edit-recipe.ts
+++ src/pages/edit-recipe/edit-recipe.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {NavParams} from "ionic-angular";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'page-edit-recipe',
@@ -8,13 +9,26 @@ import {NavParams} from "ionic-angular";
 export class EditRecipePage implements OnInit{
 
   mode = 'New';
-
   selectOptions = ['Easy', 'Medium', 'Hard'];
+  recipeForm: FormGroup;
 
   constructor(private navParams: NavParams) {
   }
 
   ngOnInit() {
     this.mode = this.navParams.get('mode');
+    this.initializeForm();
+  }
+
+  onSubmit() {
+    console.log(this.recipeForm);
+  }
+
+  private initializeForm() {
+    this.recipeForm = new FormGroup({
+        'title': new FormControl(null, Validators.required),
+        'description': new FormControl(null, Validators.required),
+        'difficulty': new FormControl('Medium', Validators.required)
+    })
   }
 }
