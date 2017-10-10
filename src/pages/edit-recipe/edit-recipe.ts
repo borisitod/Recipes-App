770aa787a69de2c5494cf16d070e5a898c94715b
Boris <boris.chen@kdis.com>
2017-10-10 17:42:32 +1100

    create an actionsheet

--- src/pages/edit-recipe/edit-recipe.ts
+++ src/pages/edit-recipe/edit-recipe.ts
@@ -1,34 +1,59 @@
 import {Component, OnInit} from '@angular/core';
-import {NavParams} from "ionic-angular";
+import {ActionSheetController, NavParams} from "ionic-angular";
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
-  selector: 'page-edit-recipe',
-  templateUrl: 'edit-recipe.html',
+    selector: 'page-edit-recipe',
+    templateUrl: 'edit-recipe.html',
 })
-export class EditRecipePage implements OnInit{
+export class EditRecipePage implements OnInit {
 
-  mode = 'New';
-  selectOptions = ['Easy', 'Medium', 'Hard'];
-  recipeForm: FormGroup;
+    mode = 'New';
+    selectOptions = ['Easy', 'Medium', 'Hard'];
+    recipeForm: FormGroup;
 
-  constructor(private navParams: NavParams) {
-  }
+    constructor(private navParams: NavParams, private actionSheetController: ActionSheetController) {
+    }
 
-  ngOnInit() {
-    this.mode = this.navParams.get('mode');
-    this.initializeForm();
-  }
+    ngOnInit() {
+        this.mode = this.navParams.get('mode');
+        this.initializeForm();
+    }
 
-  onSubmit() {
-    console.log(this.recipeForm);
-  }
+    onSubmit() {
+        console.log(this.recipeForm);
+    }
 
-  private initializeForm() {
-    this.recipeForm = new FormGroup({
-        'title': new FormControl(null, Validators.required),
-        'description': new FormControl(null, Validators.required),
-        'difficulty': new FormControl('Medium', Validators.required)
-    })
-  }
+    onManageIngredients() {
+        const actionSheet = this.actionSheetController.create({
+            title: 'What do you want to do?',
+            buttons: [
+                {
+                    text: 'Add Ingredient',
+                    handler: () => {
+
+                    }
+                },
+                {
+                    text: 'Remove all Ingredients',
+                    role: 'destructive',
+                    handler: () => {
+
+                    }
+                },
+                {
+                    text: 'Cancel',
+                    role: 'cancel'
+                }
+            ]
+        })
+    }
+
+    private initializeForm() {
+        this.recipeForm = new FormGroup({
+            'title': new FormControl(null, Validators.required),
+            'description': new FormControl(null, Validators.required),
+            'difficulty': new FormControl('Medium', Validators.required)
+        })
+    }
 }
