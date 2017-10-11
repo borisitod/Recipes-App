0042fcc44230017d90a6b385bd5208bcaf45317b
Boris <boris.chen@kdis.com>
2017-10-11 17:25:32 +1100

    create signin template and form

--- src/pages/signin/signin.ts
+++ src/pages/signin/signin.ts
@@ -1,14 +1,8 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
+import {NgForm} from "@angular/forms";
 
-/**
- * Generated class for the SigninPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
-@IonicPage()
 @Component({
   selector: 'page-signin',
   templateUrl: 'signin.html',
@@ -18,8 +12,8 @@ export class SigninPage {
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad SigninPage');
+  onSignin(form: NgForm) {
+    console.log(form.value);
   }
 
 }
