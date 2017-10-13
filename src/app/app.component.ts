ddac7cbf9e6f702797b5fbf961cb41ab7c1eba5d
Boris <boris.chen@kdis.com>
2017-10-13 16:13:40 +1100

    finish logout function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,26 @@
 import {Component, ViewChild} from '@angular/core';
-import {MenuController, NavController, Platform} from 'ionic-angular';
+import {AlertController, LoadingController, MenuController, NavController, Platform} from 'ionic-angular';
 import {StatusBar} from '@ionic-native/status-bar';
 import {SplashScreen} from '@ionic-native/splash-screen';
 import {TabsPage} from "../pages/tabs/tabs";
 import {SigninPage} from "../pages/signin/signin";
 import {SignupPage} from "../pages/signup/signup";
 import firebase from 'firebase';
+import {AuthService} from "../services/auth";
 
 
 @Component({
     templateUrl: 'app.html'
 })
 export class MyApp {
-    tabsPage: any = TabsPage;
+    rootPage: any = TabsPage;
     signinPage = SigninPage;
     signupPage = SignupPage;
     isAuthenticated = false;
     @ViewChild('nav') nav: NavController;
 
-    constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private menuCtrl: MenuController) {
+    constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private menuCtrl: MenuController, private authService: AuthService,
+                private loadingCtrl: LoadingController, private alertCtrl: AlertController) {
         firebase.initializeApp({
             apiKey: "AIzaSyCyCplWnFwPJfIncSTtHwAGcFC1l3P7dFs",
             authDomain: "ionic-recipesbook.firebaseapp.com"
@@ -26,10 +28,10 @@ export class MyApp {
         firebase.auth().onAuthStateChanged(user => {
             if (user) {
                 this.isAuthenticated = true;
-                this.nav.setRoot(this.tabsPage);
+                this.rootPage = TabsPage;
             } else {
                 this.isAuthenticated = false;
-                this.nav.setRoot(this.signinPage);
+                this.rootPage = SigninPage;
             }
         });
         platform.ready().then(() => {
@@ -46,6 +48,25 @@ export class MyApp {
     }
 
     onLogout() {
+        const loading = this.loadingCtrl.create({
+            content: 'Logging you out...'
+        });
+        loading.present();
+        this.authService.logout()
+            .then(data => {
+                loading.dismiss();
+                this.menuCtrl.close();
+                this.nav.setRoot(SigninPage);
+            })
+            .catch(error => {
+                loading.dismiss();
+                const alert = this.alertCtrl.create({
+                    title: 'Signup failed',
+                    message: error.message,
+                    buttons: ['Ok']
+                });
+                alert.present();
+            })
 
     }
 }
