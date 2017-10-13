2f1f033f0c8c9368cf93bf44c5a6af64f5853e15
Boris <boris.chen@kdis.com>
2017-10-13 15:48:49 +1100

    shopping a spinner and alert for success and failure

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {NgForm} from "@angular/forms";
 import {AuthService} from "../../services/auth";
+import {AlertController, LoadingController} from "ionic-angular";
 
 @Component({
   selector: 'page-signup',
@@ -8,15 +9,27 @@ import {AuthService} from "../../services/auth";
 })
 export class SignupPage {
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService, private loadingCtrl: LoadingController, private alertCtrl: AlertController) {
   }
 
   onSignup(form: NgForm) {
+    const loading = this.loadingCtrl.create({
+        content: 'Siging you up...'
+    });
+    loading.present();
     this.authService.signup(form.value.email, form.value.password)
         .then((data) => {
-          console.log(data);
+          loading.dismiss();
         })
-        .catch(error => console.log(error));
+        .catch(error => {
+            loading.dismiss();
+            const alert = this.alertCtrl.create({
+                title: 'Signup failed',
+                message: error.message,
+                buttons: ['Ok']
+            });
+            alert.present();
+        });
   }
 
 }
